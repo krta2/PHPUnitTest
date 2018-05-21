fec3293a4c221f433b0662fd19846e2c28066ec4
woohyun <krta22@gmail.com>
2018-05-21 13:56:08 +0900

    Add phpunit

---
+++ RestaurantCheckTest.php
@@ -0,0 +1,14 @@
+<?php
+require 'RestaurantCheck.php';
+
+class RestaurantCheckTest extends PHPUnit_Framework_TestCase
+{
+    public function testWithTaxAndTip()
+    {
+        $meal = 100;
+        $tax = 10;
+        $tip = 20;
+        $result = restaurant_check($meal, $tax, $tip);
+        $this->assertEquals(130, $result);
+    }
+}
