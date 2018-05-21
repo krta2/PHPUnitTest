09ffeef33b44bdc0b880cb0c3012224f82516bca
krta2 <krta22@gmail.com>
2018-05-21 16:41:31 +0900

    TDD

--- RestaurantCheckTest.php
+++ RestaurantCheckTest.php
@@ -34,4 +34,24 @@ class RestaurantCheckTest extends TestCase
         $expectedTax = $meal * ($tax / 100);
         $this->assertEquals($checkWithTax, $checkWithoutTax + $expectedTax);
     }
+    
+    public function testTipShouldIncludeTax()
+    {
+        $meal = 100;
+        $tax = 10;
+        $tip = 10;
+        // 4번째 인수가 true면 부가세 포함 금액을 기준으로 팁을 계산한다.
+        $result = restaurant_check($meal, $tax, $tip, true);
+        $this->assertEquals(121, $result);
+    }
+    
+    public function testTipShouldNotIncludeTax()
+    {
+        $meal = 100;
+        $tax = 10;
+        $tip = 10;
+        // 4번째 인수가 false면 부가세 제외 금액을 기준으로 팁을 계산한다.
+        $result = restaurant_check($meal, $tax, $tip, false);
+        $this->assertEquals(120, $result);
+    }
 }
