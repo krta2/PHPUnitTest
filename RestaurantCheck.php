57dfbcc56cc5c4ee7bba84894aa76a52b03bb208
krta2 <krta22@gmail.com>
2018-05-21 17:06:15 +0900

    Add includeTaxInTip

--- RestaurantCheck.php
+++ RestaurantCheck.php
@@ -1,8 +1,14 @@
 <?php
-function restaurant_check($meal, $tax, $tip)
+function restaurant_check($meal, $tax, $tip, $includeTaxInTip = false)
 {
     $tax_amount = $meal * ($tax / 100);
-    $tip_amount = $meal * ($tip / 100);
+    
+    if ($includeTaxInTip)
+        $tipBase = $meal + $tax_amount;
+    else
+        $tipBase = $meal;
+    
+    $tip_amount = $tipBase * ($tip / 100);
     $total_amount = $meal + $tax_amount + $tip_amount;
     
     return $total_amount; 
